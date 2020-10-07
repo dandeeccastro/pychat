1149396b88cbdc00baec528c73b5cd6e54605e99
Danilo <dundeepessoal@gmail.com>
2020-10-07 12:43:11 -0300

    Server refatorado para trabalhar com messager

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import select
 import sys
 
 from user import User
+from messager import Messager
 
 MAX_CLIENTS = 5
 
@@ -12,7 +13,7 @@ class CentralServer:
     def __init__(self,host,port):
         self.host = host
         self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.messager = Messager()
         self.users = dict() # username => User object
         self.chats = [] # array de usernames
         self.inputs = [sys.stdin]
@@ -21,10 +22,9 @@ class CentralServer:
     # Iniciando socket e ouvindo usuários
     def start_server(self):
         print("> [server::start_server] starting server")
-        self.sock.bind((self.host,self.port))
-        self.sock.listen(MAX_CLIENTS)
-        self.sock.setblocking(False)
-        self.inputs.append(self.sock)
+        self.messager.host_connection(self.host,self.port)
+        self.messager.set_blocking_connections(False)
+        self.inputs.append(self.messager.get_sock())
         print("> [server::start_server] started server")
 
     # Rodando servidor e lidando com as conexões 
@@ -32,22 +32,24 @@ class CentralServer:
         while True:
             read, write, execute = select.select(self.inputs,[],[])
             for command in read:
-                if command == self.sock:
+                if command == self.messager.get_sock():
 
-                    new_sock, addr = self.sock.accept()
-                    login_message = str(new_sock.recv(1024),encoding='utf-8').split() # Pego a mensagem inicial de conexão
+                    new_messager = self.messager.accept()
+                    login_message = self.messager.receive()
+                    login_message = login_message.split()
                     print('> [server::run] new user ' + login_message[1] + ' detected')
 
                     if login_message[0] == 'NEW': # Usuário novo no sistema
                         while login_message[1] in self.users.keys():
-                            new_sock.send(bytes("USERNAME ALREADY EXISTS",encoding='utf-8'))
-                            login_message = str(new_sock.recv(1024),encoding='utf-8').split()
-                        new_sock.send(bytes("REGISTERED",encoding='utf-8'))
-                        self.new_connection(login_message[1],new_sock)
+                            login_message = self.messager.send_and_receive("USERNAME ALREADY EXISTS")
+                            login_message = login_message.split()
+                        self.messager.send("REGISTERED")
+
+                        self.new_connection(login_message[1],new_messager) 
 
                     elif login_message[0] == 'OLD': # Usuário retornando de um chat
                         print("> [server::run] user " + login_message[1] + " is returning")
-                        self.returning_connection(login_message[1],new_sock)
+                        self.returning_connection(login_message[1],new_messager)
 
                 elif command == sys.stdin:
                     adm_command = input()
@@ -75,20 +77,20 @@ class CentralServer:
                 print("> [server::handle_admin_command.users_online()] " + user.username)
 
     # Lida com novos usuários entrando no servidor
-    def new_connection(self,username,sock):
+    def new_connection(self,username,messager):
         print("> [server::new_connection] new connection detected")
-        self.users[username] = User(username,sock)
-        self.inputs.append(sock)
-        # sock.send(bytes(self.show_available_chats(),encoding='utf-8'))
+        self.users[username] = User(username,messager)
+        self.inputs.append(messager.get_sock())
+        # messager.send(bytes(self.show_available_chats(),encoding='utf-8'))
         print("> [server::new_connection] new connection solved")
 
     # Lida com usuários reconectando de chats
-    def returning_connection(self,username,sock):
+    def returning_connection(self,username,messager):
         if username in self.chats:
             self.chats.remove(username)        
-        self.users[username] = User(username,sock)
-        self.inputs.append(sock)
-        sock.send(bytes(self.show_available_chats(),encoding='utf-8'))
+        self.users[username] = User(username,messager)
+        self.inputs.append(messager.get_sock())
+        messager.send(self.show_available_chats())
 
     # Gera a string com os chats existentes
     def show_available_chats(self):
@@ -106,39 +108,38 @@ class CentralServer:
 
     # Lida com mensagens de socks conectados. Se não há mensagem, removemos dos inputs, mas se há, 
     # lidamos com ela usando execute_command
-    def handle_request(self,sock):
-        message = sock.recv(1024)
+    def handle_request(self,messager):
+        message = messager.receive()
         if not message:
-            self.inputs.remove(sock)
-            sock.close()
+            self.inputs.remove(messager.get_sock())
+            messager.close()
         else:
-            message = str(message, encoding='utf-8')
             message = message.split()
-            self.execute_command(sock,message)
+            self.execute_command(messager,message)
 
     # Dependendo da mensagem, essa função encaminha para a função responsável pela resolução do comando
-    def execute_command(self,sock,message):
+    def execute_command(self,messager,message):
         if message[0] == 'create_chat':
-            self.create_new_chat(sock)
+            self.create_new_chat(messager)
         elif message[0] == 'connect':
-            self.connect_to_chat(sock,message[1])
+            self.connect_to_chat(messager,message[1])
         elif message[0] == 'close':
             print("> [server::execute_command] conexão vai ser fechada")
-            self.close_connection(sock,message[1])
+            self.close_connection(messager,message[1])
 
     # Adiciona o usuário na lista de chats, passa para ele o par host port no qual ele hosteara seu chat 
     # e atualiza as estruturas de dados para que o servidor não espere conexões vindas do soquete dele
-    def create_new_chat(self,sock):
+    def create_new_chat(self,messager):
         used_locations = [(self.host,self.port)]
         # Seleciono o usuário igualando via sock
         for user in self.users.values():
             if user.username in self.chats:
                 used_locations.append(user.chat_info) # armazeno par de host e port já utilizado pelo sistema
-            if user.sock == sock:
+            if user.messager == messager:
                 request_user = user
 
         self.chats.append(request_user.username)
-        chat_location = request_user.sock.getsockname()
+        chat_location = request_user.messager.get_pair_host_port()
         while chat_location in used_locations:
             chat_location = (chat_location[0], chat_location[1] + 1)
             if chat_location not in used_locations:
@@ -146,21 +147,22 @@ class CentralServer:
                 break
 
         self.users[request_user.username].chat_info = chat_location
-        sock.send(bytes(chat_location[0] + ' ' + str(chat_location[1]), encoding='utf-8'))
-        self.emit_available_chats()
+        messager.send(chat_location[0] + ' ' + str(chat_location[1]))
+
+        self.messager.emit_message(self.get_available_chats(),[user.messager for user in self.users.values()])
 
     # Envia a dupla host port do chat, se existir. Senão, envia DENIED
-    def connect_to_chat(self,sock,username):
+    def connect_to_chat(self,messager,username):
         if username in self.chats:
             target = self.users[username].chat_info
-            sock.send(bytes(target[0] + ' ' + str(target[1]), encoding='utf-8'))
+            messager.send(target[0] + ' ' + str(target[1]))
         else:
-            sock.send(bytes('DENIED',encoding='utf-8'))
+            messager.send('DENIED')
 
     # Remove o usuário das estruturas de dados
-    def close_connection(self,sock,username):
+    def close_connection(self,messager,username):
         print("> [server::close_connection] " + username + " is logging out")
-        sock.send(bytes("Goodbye",encoding='utf-8'))
+        messager.send("Goodbye")
         for uid in self.users.keys():
             if uid == username:
                 self.users.pop(uid)
